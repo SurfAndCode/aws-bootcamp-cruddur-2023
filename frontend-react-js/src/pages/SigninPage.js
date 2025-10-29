0c02243c588c9b18ace743a35a0035ae3d4b44d5
Zoltan Oasz <zoltan.olasz76@gmail.com>
2025-10-29 13:06:30 +1000

    commenting out localstorage

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -19,8 +19,8 @@ export default function SigninPage() {
   try {
     Auth.signIn(email, password)
       .then(user => {
-        console.log('user',user)
-        localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken)
+        // console.log('user',user)
+        // localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken)
         window.location.href = "/"
       })
       .catch(err => { console.log('Error!', err) });
